65e40b0ccc87652c27f49fa90a16c6cb78929d25
DavidTheMe <71494143+DavidTheMe@users.noreply.github.com>
2025-05-19 16:16:58 +0200

    la till middleware som kollar om email och password är valid

--- server.js
+++ server.js
@@ -19,3 +19,41 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+// Middleware
+
+function validateLogin(req, res, next) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if(req.body.email && req.body.email != "")
+  {
+    if (!emailRegex.test(req.body.email))
+    {
+      res.status(400).send("The email address you entered is not a valid email");
+    }
+  }
+  else
+  {
+    res.status(400).send("You need to enter an email");
+  }
+
+  if(req.body.password && req.body.password != "")
+  {
+    if(req.body.password.length <= 6)
+    {
+      res.status(400).send("Password is invalid (it needs to be at least 6 characters)");
+    }
+  }
+  else
+  {
+    res.status(400).send("You need to enter a password");
+  }
+
+  next();
+}
+
+
+app.post('/login', validateLogin, (req, res) => {
+  res.send("request is valid");
+});
